a751c2264fac1687f359f63ab3b1e827524fb509
Alfred Wang <47693725+xiangyun-wang@users.noreply.github.com>
2022-09-14 17:06:42 -0400

    update

--- Assignment 1/ecse543_a1p1.swift
+++ Assignment 1/ecse543_a1p1.swift
@@ -9,6 +9,8 @@ import Foundation
  * Input: Lower Triangular Matrix, RHS vector
  * Output: Matrix solution
  */
+print ("Hello Swift")
+
 func forward(Lower : [[Double]], b : [Double]) -> [Double]? {
     let n = b.count                                     // matrix dimension
     var output = Array(repeating: 0.0, count: n)        // output vector initialization
@@ -77,26 +79,72 @@ func cholesky_decomposition(A : [[Double]]) -> [[Double]]? {
                 extra_term += output[i][k] * output[j][k]
                 k += 1;
             }
+            // ----- implies that A is not a symmetric positive definite matrix -----
+            if A[i][j] - extra_term <= 0 {
+                return nil
+            }
+            // ----- end of check for SPD ------
             if i == j {
                 output[i][j] = sqrt(A[i][i] - extra_term)
             } else {
                 output[i][j] = 1.0 / output[j][j] * (A[i][j] - extra_term)
             }
-            if (output[i][j] == 0) {
-                return nil
-            }
+            // if (output[i][j] == 0) {
+            //     return nil
+            // }
         }
     }
     return output
+}
+
+func find_half_band(A: [[Double]]) -> Int {
+    let n = A.count
+    var band = 0
+    for i in 0...n-1 {
+        for j in 0...i {
+            
+        }
+    }
+}
+
+func chelesky_decomposition_optimized(A: [[Double]]) -> [Double]? {
+   let n = A.count
+    var extra_term: Double
+    var output = Array(repeating: Array(repeating: 0.0, count: n), count: n)
+    var k: Int
+    for i in 0...n-1 {
+        for j in 0...i {
+            extra_term = 0
+            k = 0
+            while (k<j){
+                extra_term += output[i][k] * output[j][k]
+                k += 1;
+            }
+            // ----- implies that A is not a symmetric positive definite matrix -----
+            if A[i][j] - extra_term <= 0 {
+                return nil
+            }
+            // ----- end of check for SPD ------
+            if i == j {
+                output[i][j] = sqrt(A[i][i] - extra_term)
+            } else {
+                output[i][j] = 1.0 / output[j][j] * (A[i][j] - extra_term)
+            }
+            // if (output[i][j] == 0) {
+            //     return nil
+            // }
+        }
+    }
+    return output 
 }
 
 func cheleskySolver(A: [[Double]], b: [Double]) -> [Double]? {
     return nil
 }
 
-func checkPosDef(A: [[Double]]) -> Bool {
-    return false
-}
+// func PSD_Check(A: [[Double]]) -> Bool {
+//     return false
+// }
 
 func dotproduct_vector(A: [[Double]], b:[Double]) -> [Double]? {
     return nil
@@ -104,4 +152,12 @@ func dotproduct_vector(A: [[Double]], b:[Double]) -> [Double]? {
 
 func transpose(A: [[Double]]) -> [[Double]]? {
     return nil
+}
+
+func PSD_Generation(size: Int) -> [[Double]]? {
+    return nil
+}
+
+func Read_Circuit_File(Dir: String) {
+
 }
