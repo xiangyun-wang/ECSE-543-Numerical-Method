ea86508459a6c4ef0fde2c9b8cc430a5dd216240
Alfred Wang <47693725+xiangyun-wang@users.noreply.github.com>
2022-10-11 01:26:01 -0400

    update

--- Assignment 1/ecse543_a1p1.swift
+++ Assignment 1/ecse543_a1p1.swift
@@ -55,147 +55,16 @@ func backward(Upper : [[Double]], b : [Double]) -> [Double]? {
     return output  
 }
 
-/*
- * Input: Matrix A to be decomposed
- * Output: Matrix L, where A = L*transpose(L)
- * 
- */
-func cholesky_decomposition(A : [[Double]]) -> [[Double]]? {
-    let n = A.count
-    var extra_term: Double
-    var output = Array(repeating: Array(repeating: 0.0, count: n), count: n)
-    var k: Int
-    for i in 0...n-1 {
-        for j in 0...i {
-            extra_term = 0
-            k = 0
-            while (k<j){
-                extra_term += output[i][k] * output[j][k]
-                k += 1;
-            }
-            // ----- implies that A is not a symmetric positive definite matrix -----
-            if (i == j) && (A[i][j] - extra_term < 0) {
-                return nil
-            }
-            // ----- end of check for SPD ------
-            if i == j {
-                output[i][j] = sqrt(A[i][i] - extra_term)
-            } else {
-                output[i][j] = 1.0 / output[j][j] * (A[i][j] - extra_term)
-            }
-            // if (output[i][j] == 0) {
-            //     return nil
-            // }
-        }
+// element-wise subtraction for two matrices
+func subtract(A: [Double], B: [Double]) -> [Double]{
+    var output = A
+    for i in 0...A.count-1{
+        output[i] = A[i] - B[i]
     }
     return output
 }
 
-func find_half_band(A: [[Double]]) -> Int {
-    var band: Int = 0
-    var N_Zero: Int = 0
-    for i in 0...A.count-1 {
-        N_Zero = 0
-        for j in 0...i{
-            if A[i][j] == 0{
-                N_Zero += 1;
-            }else{
-                break
-            }
-        }
-        if i+1-N_Zero > band {
-            band = i+1-N_Zero        
-        }
-    }
-
-    return band
-    // initially set band = 0, 
-    // loop over all rows
-    // counter add until first non-zero element is met in a row (N_zero)
-    // if i+1-N_zero > band, then band = i+1-N_zero
-    // return band
-}
-
-// -----------------------------------------------------------------
-func cholesky_decomposition_optimized(A: [[Double]], RHS: [Double]) -> ([[Double]]?, [Double]?) {
-    let n = A.count
-    var A_a = A
-    var RHS_a = RHS
-
-    for j in 0...n-1 {
-        if A_a[j][j] <= 0 {
-            return (nil, nil)
-        }
-        A_a[j][j] = sqrt(A_a[j][j])
-        RHS_a[j] = RHS_a[j]/A_a[j][j]
-        if j == n-1 {
-            break
-        }
-
-        for i in j+1...n-1 {   // for optimized, it should be i in j+1...j+(band-1) or j+1...n-1 if j+(band-1) > n-1
-            A_a[i][j] = A_a[i][j]/A_a[j][j]
-            RHS_a[i] = RHS_a[i] - A_a[i][j]*RHS_a[j]
-            for k in j+1...i {
-                A_a[i][k] = A_a[i][k] - A_a[i][j]*A_a[k][j]
-            }
-        }
-    }
-    return (A_a, RHS_a)
-}
-// -----------------------------------------------------------------
-
-
-func cholesky_decomposition_optimized_bandwidth_forward(A: [[Double]], RHS: [Double], band: Int) -> ([[Double]]?, [Double]?) {
-    let n = A.count
-    var A_a = A
-    var RHS_a = RHS
-    var i_range: Int
-
-    for j in 0...n-1 {
-        if A_a[j][j] <= 0 {
-            return (nil, nil)
-        }
-        A_a[j][j] = sqrt(A_a[j][j])
-        RHS_a[j] = RHS_a[j]/A_a[j][j]
-        if j == n-1 {
-            break
-        }
-        if j+band-1 > n-1 {
-            i_range = n-1
-        }else{
-            i_range = j+band-1
-        }
-        for i in j+1...i_range {   // for optimized, it should be i in j+1...j+(band-1) or j+1...n-1 if j+(band-1) > n-1
-            A_a[i][j] = A_a[i][j]/A_a[j][j]
-            RHS_a[i] = RHS_a[i] - A_a[i][j]*RHS_a[j]
-            for k in j+1...i {
-                A_a[i][k] = A_a[i][k] - A_a[i][j]*A_a[k][j]
-            }
-        }
-    }
-    return (A_a, RHS_a)
-}
-
-func choleskySolver(A: [[Double]], b: [Double]) -> [Double]? {
-
-    let (L, y) = cholesky_decomposition_optimized(A: A, RHS: b)
-    if L == nil {
-        return nil
-    }
-
-    return backward(Upper: transpose(A: L!), b: y!) 
-}
-
-func choleskySolver_Optimized(A: [[Double]], b: [Double]) -> [Double]? {
-    let band = find_half_band(A: A) 
-    print("band: " + String(band))
-    let (L, y) = cholesky_decomposition_optimized_bandwidth_forward(A: A, RHS: b, band: band)
-    if L == nil {
-        return nil
-    }
-    return backward(Upper: transpose(A: L!), b: y!) 
-}
-
+// dot product of matrix and a vector
 func dotproduct_vector(A: [[Double]], b:[Double]) -> [Double] {
     var output = Array(repeating: 0.0, count: A.count)
     for i in 0...A.count-1 {
@@ -206,7 +75,7 @@ func dotproduct_vector(A: [[Double]], b:[Double]) -> [Double] {
     return output
 }
 
-// need to check
+// dot product with two matrices
 func dotproduct_matrix(A: [[Double]], B: [[Double]]) -> [[Double]]{
     var output:[[Double]] = []
     let B_transpose = transpose(A: B)
@@ -216,6 +85,7 @@ func dotproduct_matrix(A: [[Double]], B: [[Double]]) -> [[Double]]{
     return transpose(A: output)
 }
 
+// transpose of a matrix
 func transpose(A: [[Double]]) -> [[Double]]{
     var output = Array(repeating: Array(repeating: 0.0, count: A.count), count: A[0].count)
     for i in 0...A.count-1 {
@@ -226,9 +96,117 @@ func transpose(A: [[Double]]) -> [[Double]]{
     return output
 }
 
-func PSD_Generation(size: Int) -> [[Double]]? {
-    return nil
+// func cholesky_decomposition(A : [[Double]]) -> [[Double]]? {
+//     let n = A.count
+//     var extra_term: Double
+//     var output = Array(repeating: Array(repeating: 0.0, count: n), count: n)
+//     var k: Int
+//     for i in 0...n-1 {
+//         for j in 0...i {
+//             extra_term = 0
+//             k = 0
+//             while (k<j){
+//                 extra_term += output[i][k] * output[j][k]
+//                 k += 1;
+//             }
+//             // ----- implies that A is not a symmetric positive definite matrix or the matrix is singular-----
+//             if (i == j) && (A[i][j] - extra_term < 0) {
+//                 return nil
+//             }
+//             // ----- end of check for SPD ------
+//             if i == j {
+//                 output[i][j] = sqrt(A[i][i] - extra_term)
+//             } else {
+//                 output[i][j] = 1.0 / output[j][j] * (A[i][j] - extra_term)
+//             }
+//             // if (output[i][j] == 0) {
+//             //     return nil
+//             // }
+//         }
+//     }
+//     return output
+// }
+
+
+
+/*
+ * Input: Matrix A to be decomposed
+ * Output: Decomposed matrix (L), Updated RHS vector (decomposition and forward substitution comnbine in this function)
+ * 
+ * Followed algorithm as shown in lecture slides
+ */
+func cholesky_decomposition(A: [[Double]], RHS: [Double]) -> ([[Double]]?, [Double]?) {
+    let n = A.count
+    var A_a = A
+    var RHS_a = RHS
+
+    for j in 0...n-1 {
+        if A_a[j][j] <= 0 {     // not symmetric positive definite or singular
+            return (nil, nil)
+        }
+        A_a[j][j] = sqrt(A_a[j][j])     // update diagonal element first
+        RHS_a[j] = RHS_a[j]/A_a[j][j]   // update corresponding right hand size
+        if j == n-1 {   // if it's the last column, only the diagonal element need to be updated, break directly
+            break
+        }
+
+        for i in j+1...n-1 {   // update elements below current diagonal element (column)
+            A_a[i][j] = A_a[i][j]/A_a[j][j]
+            RHS_a[i] = RHS_a[i] - A_a[i][j]*RHS_a[j]
+            for k in j+1...i {
+                A_a[i][k] = A_a[i][k] - A_a[i][j]*A_a[k][j]
+            }
+        }
+    }
+    return (A_a, RHS_a)
 }
+
+
+func choleskySolver(A: [[Double]], b: [Double]) -> [Double]? {
+    // decompose matrix
+    let (L, y) = cholesky_decomposition(A: A, RHS: b)
+    // check if decomposition is successful (not symmetric positive definite or singular)
+    if L == nil {
+        return nil
+    }
+    // the returned "y" above has already been done forward substitution
+    // only backward substitution is needed
+    return backward(Upper: transpose(A: L!), b: y!) 
+}
+
+func SPD_Generation(size: Int) -> [[Double]]? {
+    var M: [[Double]] = Array(repeating: Array(repeating: 0, count: size), count: size)
+    // the following for-loops fill up the lower triangular M without 0s. 
+    // this ensures that each line is unique and without zeros, which garantees non-singularity (must have a solution)
+    for i in 0...size-1{
+        for j in 0...i{
+            M[i][j] = Double(Int.random(in: 1...100))
+        }
+    }
+    
+    return dotproduct_matrix(A: M, B: transpose(A: M))
+}
+
+func test_cholesky(size: Int) -> Void {
+    let A = SPD_Generation(size: size)
+    var x: [Double] = Array(repeating: 0.0, count: size)
+    for i in 0...size-1{
+        x[i] = Double(Int.random(in: 0...100))
+    }
+    let b = dotproduct_vector(A: A!, b: x)
+    let x_cholesky = choleskySolver(A: A!, b: b)
+    print("The invented x is: ")
+    print(x)
+    // for i in 0...size-1{
+    //     print(x[i])
+    // }
+    print("The recalculated x is: ")
+    print(x_cholesky!)
+    // for i in 0...size-1{
+    //     print(x_cholesky![i])
+    // }
+}
+
 
 func Read_Circuit_File(Dir: String) -> [(String, String, Double, Double, Double)] {
     var output: [String]
@@ -287,13 +265,7 @@ func Extract_Circuit(circuit: [(String, String, Double, Double, Double)]) -> ([[
     return (y,E,J)
 }
 
-func subtract(A: [Double], B: [Double]) -> [Double]{
-    var output = A
-    for i in 0...A.count-1{
-        output[i] = A[i] - B[i]
-    }
-    return output
-}
+
 
 func Simple_Circuit_A_b(Matrix_Dir: String, Circuit_Dir: String) -> ([[Double]], [Double]){
     
@@ -315,6 +287,73 @@ func Solve_Circuit(Circuit: ([[Double]], [Double]), Optimize: Bool) -> [Double]{
     }
 }
 
+
+// ------------------------- part 2 ------------------------------
+
+func choleskySolver_Optimized(A: [[Double]], b: [Double]) -> [Double]? {
+    let band = find_half_band(A: A) 
+    //print("N: " +  String(Int(sqrt(Double(b.count+1)))) + ", Matrix Size: " + String(b.count) + ", Band: " + String(band))
+    let (L, y) = cholesky_decomposition_optimized_bandwidth_forward(A: A, RHS: b, band: band)
+    if L == nil {
+        return nil
+    }
+    return backward(Upper: transpose(A: L!), b: y!) 
+}
+
+func find_half_band(A: [[Double]]) -> Int {
+    // initially set band = 0, 
+    // loop over all rows
+    // counter add until first non-zero element is met in a row (N_zero)
+    // if i+1-N_zero > band, then band = i+1-N_zero
+    // return band
+    var band: Int = 0
+    var N_Zero: Int = 0
+    for i in 0...A.count-1 {
+        N_Zero = 0
+        for j in 0...i{
+            if A[i][j] == 0{
+                N_Zero += 1;
+            }else{
+                break
+            }
+        }
+        if i+1-N_Zero > band {
+            band = i+1-N_Zero        
+        }
+    }
+    return band
+}
+
+func cholesky_decomposition_optimized_bandwidth_forward(A: [[Double]], RHS: [Double], band: Int) -> ([[Double]]?, [Double]?) {
+    let n = A.count
+    var A_a = A
+    var RHS_a = RHS
+    var i_range: Int
+
+    for j in 0...n-1 {
+        if A_a[j][j] <= 0 {
+            return (nil, nil)
+        }
+        A_a[j][j] = sqrt(A_a[j][j])
+        RHS_a[j] = RHS_a[j]/A_a[j][j]
+        if j == n-1 {
+            break
+        }
+        if j+band-1 > n-1 {
+            i_range = n-1
+        }else{
+            i_range = j+band-1
+        }
+        for i in j+1...i_range {   // for optimized, it should be i in j+1...j+(band-1) or j+1...n-1 if j+(band-1) > n-1
+            A_a[i][j] = A_a[i][j]/A_a[j][j]
+            RHS_a[i] = RHS_a[i] - A_a[i][j]*RHS_a[j]
+            for k in j+1...i {
+                A_a[i][k] = A_a[i][k] - A_a[i][j]*A_a[k][j]
+            }
+        }
+    }
+    return (A_a, RHS_a)
+}
 
 func Mesh_Circuit_Generation(N: Int, R: Double, V: Double) -> ([[Double]], [Double]) {
     let N_R = N*(N-1)*2
@@ -431,8 +470,8 @@ func finite_difference_SOR(h: Double, w: Double, threshold: Double) -> ([[Double
     let inner_voltage = 110.0
     let inner_x = Int(0.06/h)
     let inner_y = Int(0.08/h)
-    let free_top = 0.1
-    let free_right = 0.1
+    //let free_top = 0.1
+    //let free_right = 0.1
     let x_range_discret = Int(0.1/h)
     let y_range_discret = Int(0.1/h)
     var grid_potential = Array(repeating: Array(repeating: 0.0, count: y_range_discret+1), count: x_range_discret+1)
@@ -448,10 +487,6 @@ func finite_difference_SOR(h: Double, w: Double, threshold: Double) -> ([[Double
     for i in inner_x...grid_potential.count-1 {
         grid_potential[i][inner_y] = inner_voltage
     }
-
-    print(grid_potential)
-
-
     
     while (residue > threshold) {
         for i in 1...x_range_discret{
@@ -479,8 +514,8 @@ func finite_difference_jacobi(h: Double, threshold: Double) -> ([[Double]], Int)
     let inner_voltage = 110.0
     let inner_x = Int(0.06/h)
     let inner_y = Int(0.08/h)
-    let free_top = 0.1
-    let free_right = 0.1
+    //let free_top = 0.1
+    //let free_right = 0.1
     let x_range_discret = Int(0.1/h)
     let y_range_discret = Int(0.1/h)
     var grid_potential = Array(repeating: Array(repeating: 0.0, count: y_range_discret+1), count: x_range_discret+1)
@@ -522,11 +557,86 @@ func finite_difference_jacobi(h: Double, threshold: Double) -> ([[Double]], Int)
 }
 //-------------------------- part 3 end ---------------------------
 
-let (potential, iter) = finite_difference_SOR(h: 0.02, w: 1.9, threshold: 0.00001)
-print(potential)
-print(iter)
+// let (potential, iter) = finite_difference_SOR(h: 0.02, w: 1.9, threshold: 0.00001)
+// print(potential)
+// print(iter)
 
-let (potential_j, iter_j) = finite_difference_jacobi(h: 0.02, threshold: 0.00001)
-print(potential_j)
-print(iter_j)
-//print(iter_j)j)
+// let (potential_j, iter_j) = finite_difference_jacobi(h: 0.02, threshold: 0.00001)
+// print(potential_j)
+// print(iter_j)
+// //print(iter_j)j)
+
+// ------------ part 1 test ---------------
+// test_cholesky(size: 10)
+// print("Circuit 1 solution: ")
+// print(Solve_Circuit(Circuit: (Simple_Circuit_A_b(Matrix_Dir: "test_1_A_reduced.txt", Circuit_Dir: "test_1_circuit.txt")), Optimize: false))
+// print("Circuit 2 solution: ")
+// print(Solve_Circuit(Circuit: (Simple_Circuit_A_b(Matrix_Dir: "test_2_A_reduced.txt", Circuit_Dir: "test_2_circuit.txt")), Optimize: false))
+// print("Circuit 3 solution: ")
+// print(Solve_Circuit(Circuit: (Simple_Circuit_A_b(Matrix_Dir: "test_3_A_reduced.txt", Circuit_Dir: "test_3_circuit.txt")), Optimize: false))
+// print("Circuit 4 solution: ")
+// print(Solve_Circuit(Circuit: (Simple_Circuit_A_b(Matrix_Dir: "test_4_A_reduced.txt", Circuit_Dir: "test_4_circuit.txt")), Optimize: false))
+// print("Circuit 5 solution: ")
+// print(Solve_Circuit(Circuit: (Simple_Circuit_A_b(Matrix_Dir: "test_5_A_reduced.txt", Circuit_Dir: "test_5_circuit.txt")), Optimize: false))
+
+// -------------part 2 test ----------------
+var Reqs = Array(repeating: 0.0, count: 14)
+var Times = Array(repeating: 0.0, count: 14)
+var Req_counter  = 0
+for i in 2...15 {
+    (Reqs[Req_counter],Times[Req_counter]) = find_Req(N: i, R: 10000, V: 100, Optimize: true)
+    Req_counter += 1
+}
+(_,Times[0]) = find_Req(N: 2, R: 10000, V: 100, Optimize: true)
+print(Times)
+
+// var test1=0.0
+// var test2 = 0.0
+// (test1,test2) = find_Req(N: 2, R: 10000, V: 100, Optimize: false)
+// print(test2)
+
+// -------------- part 3 test ------------------------
+// part (b)
+// let w = [1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9]
+// var field: [[Double]]
+// var iterations = [0,0,0,0,0,0,0,0,0,0]
+// var sixfour_value = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+// var p3_counter = 0
+// for i in w{
+//     (field,iterations[p3_counter]) = finite_difference_SOR(h: 0.02, w: i, threshold: 0.00001)
+//     sixfour_value[p3_counter] = field[Int(0.06/0.02)][Int(0.04/0.02)]
+//     p3_counter += 1
+// }
+// print(iterations)
+// print(sixfour_value)
+
+// // part (c)
+// let h = [0.02, 0.01,0.005,0.0025,0.00125]
+// var field: [[Double]]
+// var iterations = [0,0,0,0,0]
+// var sixfour_value = [0.0,0.0,0.0,0.0,0.0]
+// var p3_counter = 0
+// for i in h{
+//     (field,iterations[p3_counter]) = finite_difference_SOR(h: i, w: 1.3, threshold: 0.00001)
+//     sixfour_value[p3_counter] = field[Int(0.06/i)][Int(0.04/i)]
+//     p3_counter += 1
+// }
+// print(iterations)
+// print(sixfour_value)
+
+// part (d)
+// let h = [0.02, 0.01,0.005,0.0025,0.00125]
+// var field: [[Double]]
+// var iterations = [0,0,0,0,0]
+// var sixfour_value = [0.0,0.0,0.0,0.0,0.0]
+// var p3_counter = 0
+// for i in h{
+//     (field,iterations[p3_counter]) = finite_difference_jacobi(h: i, threshold: 0.00001)
+//     sixfour_value[p3_counter] = field[Int(0.06/i)][Int(0.04/i)]
+//     p3_counter += 1
+// }
+// print(iterations)
+// print(sixfour_value)
+
+// test_cholesky(size: 5)
+// test_cholesky(size: 10)
